4d638f9a8d40c6f3b38f6f58f21506eb781429a7
YuanCome <tanheyuan@outlook.com>
2020-12-19 23:21:46 +0800

    提交版本优化以及测试之后的代码

--- soul-spring-boot-demo/src/main/java/com/yuancome/soul/springboot/demo/controller/UserController.java
+++ soul-spring-boot-demo/src/main/java/com/yuancome/soul/springboot/demo/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.yuancome.soul.springboot.demo.controller;
 
 import com.yuancome.soul.springboot.demo.dto.UserDTO;
-import org.dromara.soul.client.springmvc.annotation.SoulSpringMvcClient;
+import org.dromara.soul.client.common.annotation.SoulClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,14 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @PostMapping("/create")
-    @SoulSpringMvcClient(path = "/user/create", desc = "创建新用户")
+    @SoulClient(path = "/user/create", desc = "创建新用户")
     public String create(@RequestBody UserDTO userDTO) {
         System.out.println(userDTO.toString());
         return "创建用户成功";
     }
 
     @GetMapping("/get")
-    @SoulSpringMvcClient(path = "/user/get", desc = "获得用户详细")
+    @SoulClient(path = "/user/get", desc = "获得用户详细")
     public UserDTO get(@RequestParam("id") Long id) {
         UserDTO userDTO = new UserDTO();
         userDTO.setAge(18);
